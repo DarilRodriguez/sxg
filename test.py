ea56c91be41a03ef14736e4c3a6d41cc0b739aa0
Daril Rodriguez <darilrodriguez.2@gmail.com>
2019-10-07 22:54:50 -0300

    [TEST] Test OPTIONAL

--- test.py
+++ test.py
@@ -3,15 +3,8 @@ from grammar import Grammar
 def main():
     grm = Grammar()
     
-    grm.load_def("dotted_name: 'name'=IDENT 'list'=*('.' 'name'=IDENT)") # a.b.c
-    grm.load_def("assign: 'name_a'=dotted_name '=' 'name_b'=dotted_name") # a.b.c = d.e.f
+    grm.load_def("assign: 'test'=['a' 'b' 'c'] 'name'=IDENT")
     
-    data = {}
-    grm.set_line("a.b.c")
-    print grm.parse('dotted_name', data), data
-    # output: True {'name': 'a', 'list': [{'name': b}, {'name': c}]}
-    
-    print grm.parse_line("a.b = b.c", 'assign')
-    # output: (True, {'name_b': {'list': [{'name': 'c'}], 'name': 'b'}, 'name_a': {'list': [{'name': 'b'}], 'name': 'a'}})
+    print grm.parse_line("a Hola", 'assign')
 
 main()
